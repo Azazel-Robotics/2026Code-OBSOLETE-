b378cf405e9f8b65820ebbdc6e9f0c9399179fcf
Azazel-Robotics <camille.malazarte516@gmail.com>
2026-04-10 09:35:28 -0400

    adjusted intake to not spin when arm is up

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -9,10 +9,13 @@ import com.revrobotics.spark.SparkLowLevel.MotorType;
 import frc.robot.Constants;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
+import frc.robot.subsystems.IntakeArm;
 
 public class Intake extends SubsystemBase {
     
     private SparkMax intakeMotor = new SparkMax(Constants.Intake.kIntakeMotor, MotorType.kBrushless);
+
+     IntakeArm passiveLimit = new IntakeArm();
 
     public Intake() {
         
@@ -20,11 +23,14 @@ public class Intake extends SubsystemBase {
         intakeConfig.smartCurrentLimit(Constants.kMaxCurrent);
         intakeConfig.idleMode(IdleMode.kBrake);
         intakeMotor.configure(intakeConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-
     }
 
     public Command spinIntake(double speed) {
-        return this.runOnce( () -> intakeMotor.set(speed));
+        if (passiveLimit.getLimitUp()){
+            return this.runOnce( () -> intakeMotor.set(0));
+        } else {
+            return this.runOnce( () -> intakeMotor.set(speed));
+        }
     }
     
 
--- src/main/java/frc/robot/subsystems/IntakeArm.java
+++ src/main/java/frc/robot/subsystems/IntakeArm.java
@@ -7,6 +7,7 @@ import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -78,6 +79,10 @@ public class IntakeArm extends SubsystemBase {
         }else{
             return this.runOnce(() -> intakeArmMotor.set(speed));
         }
+    }
+
+    public boolean getLimitUp() {
+        return limitUp.isPressed();
     }
 
 
