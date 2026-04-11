4b5742112f88415812368a5a0f8e05e58d379e12
CodingJeanine <jeaninealfaro16@gmail.com>
2026-04-11 19:27:48 -0400

    adjusted pathplanner auto to be quicker

--- src/main/java/frc/robot/commands/AutoLeftStartScoring.java
+++ src/main/java/frc/robot/commands/AutoLeftStartScoring.java
@@ -12,20 +12,29 @@ public class AutoLeftStartScoring extends SequentialCommandGroup {
      //1st path to shoot into hub
       new PathPlannerAuto("Backwards"),
         
-        //RD: Shoot all 8 balls in Hopper! I Hope
-      new WaitCommand(4.5).deadlineFor(AutoCommands.Shoot(Shooter.getInstance(), Index.getInstance(), .75, .5)),
+      //RD: Shoot all 8 balls in Hopper! I Hope
+      new WaitCommand(3).deadlineFor(AutoCommands.Shoot(Shooter.getInstance(), Index.getInstance(), .75, .5)),
       
-      new WaitCommand(1).deadlineFor(AutoCommands.Shoot(Shooter.getInstance(), Index.getInstance(), 0, .0)),
-        
+      new WaitCommand(.1).deadlineFor(AutoCommands.Shoot(Shooter.getInstance(), Index.getInstance(), 0, 0)),
+      /* 
+      //Get in Position to collect Fuel from Mid
       new PathPlannerAuto("Mid"),
 
-      new WaitCommand(1.5).deadlineFor(IntakeArm.getInstance().autoSpinArmDown(.65)),
+      //Arm Down, Arm down takes about 1.3 seconds
+      new WaitCommand(1.5).deadlineFor(IntakeArm.getInstance().spinArmDown(.65)),
+      */
 
+      //Trying to Combine 1: Get in Poisition to collect fuel form mid, 2: IntakeArm Down
+      //According to Path Planner, It clear the trench at 1.9 seconds
+      new ParallelCommandGroup(
+        new PathPlannerAuto("Mid"), 
+        new WaitCommand(1.9).andThen(new WaitCommand(1.5).deadlineFor(IntakeArm.getInstance().spinArmDown(.65)))),
+      
       new PathPlannerAuto("Intake").deadlineFor(Intake.getInstance().spinIntake(1)),
       
-      new WaitCommand(1).deadlineFor(Intake.getInstance().spinIntake(0)),
+      new WaitCommand(.1).deadlineFor(Intake.getInstance().spinIntake(0)),
 
-      new WaitCommand(1.5).deadlineFor(IntakeArm.getInstance().autoSpinArmUp(.65)),
+      new WaitCommand(.5).deadlineFor(IntakeArm.getInstance().spinArmUp(.75)),
 
       new PathPlannerAuto("Hub 1"),
 
